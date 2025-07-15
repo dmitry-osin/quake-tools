b6b717944f50fa8e05fa3e5eaaeb5ca515550619
Dmitry Osin <d@osin.pro>
2025-07-16 02:02:24 +0300

    [~] project structure

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use slint::SharedString;
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = AppWindow::new()?;
+    app.set_window_title(SharedString::from("Quake Tools"));
+    app.run()?;
+    Ok(())
+}
